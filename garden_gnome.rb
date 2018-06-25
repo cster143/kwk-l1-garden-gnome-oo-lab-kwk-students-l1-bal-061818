e2417a68d38a7f6cc4827804dab7b00571e4e37b
cster143 <cster143@gmail.com>
2018-06-25 12:53:49 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,42 @@
 # Code your instances here
+class GardenGnome
+  attr_reader :gluten_allergy, :personality
+  attr_writer :gluten_allergy
+
+
+def name= name
+    @name= name
+  end
+
+  def name
+    @name
+  end
+
+  def age= age
+    @age= age
+  end
+
+  def age
+    @age
+  end
+
+
+  def initialize (hat_color = "red")
+    @personality= "evil"
+    @hat_color= hat_color
+  end
+
+  def gnaw
+    returns "Gnawing on a tree!!!"
+  end
+
+  def shout
+    returns "GNARLY!!!"
+  end
+
+  def introduce_self
+    puts "Hello, humans, my name is #{@name}, I am #{@age}years old, and you'll rue the day you crossed me!"
+  end
+end
+
+hat_color= GardenGnome.new (initialize)
